68da028d204be409d9b017fbb43794bb28f87d97
albert20090318 <81063788+albert20090318@users.noreply.github.com>
2025-05-25 14:52:00 +0800

    enter info

--- lib/user/user_info.h
+++ lib/user/user_info.h
@@ -1,11 +1,18 @@
 #ifndef USER_INFO_H
 #define USER_INFO_H
+#include <bits/stdc++.h>
 
 struct User
 {
     int id;
-    std::string name;
+    std::string user_name;
+    std::string gender;
     int age;
+    std::string job;
+    int length;
+    int weight;
+    int height;
+    string habit;
     std::vector<std::string> conversation_record;
 };
 static std::map<int, User> user_db = {};
