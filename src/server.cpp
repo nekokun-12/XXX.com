fb80d314917bfc8ca3694ed55f9910e6846c50cf
nekokun-12 <amor755102@gmail.com>
2025-06-15 14:58:46 +0800

    just like below

--- src/server.cpp
+++ src/server.cpp
@@ -13,7 +13,7 @@ int main()
     User info;
 
     // /api/help - 顯示各項功能
-    svr.Get("/api/help", [&](const httplib::Request &req, httplib::Response &res) {
+    svr.Post("/api/help", [&](const httplib::Request &req, httplib::Response &res) {
         json j;
         j["content"] = functions;
         res.status = 200;
@@ -21,7 +21,7 @@ int main()
     });
     
     // /api/register - 接收 JSON 格式的新用戶資料，建立新用戶
-    svr.Get("/api/register", [&](const httplib::Request &req, httplib::Response &res) {
+    svr.Post("/api/register", [&](const httplib::Request &req, httplib::Response &res) {
         try {
             json req_json = json::parse(req.body);
             // 簡單驗證必要欄位是否存在
@@ -59,7 +59,7 @@ int main()
     });
     
     // /api/user_info/:id - 取得指定 ID 的用戶資訊
-    svr.Get("/api/user_info/:id", [&](const httplib::Request &req, httplib::Response &res) {
+    svr.Post("/api/user_info/:id", [&](const httplib::Request &req, httplib::Response &res) {
         int user_id = std::stoi(req.path_params.at("id"));  // 取得路徑參數 id 並轉為整數
         if (user_db.find(user_id) == user_db.end()) {
             res.status = 404;
@@ -83,7 +83,7 @@ int main()
     });
 
     // /api/change_info/:id - 修改指定用戶特定資訊
-    svr.Get("/api/users/change_info/:id", [&](const httplib::Request &req, httplib::Response &res) {
+    svr.Post("/api/users/change_info/:id", [&](const httplib::Request &req, httplib::Response &res) {
         int user_id = std::stoi(req.path_params.at("id"));
         if (user_db.find(user_id) == user_db.end()) {
             res.status = 404;
@@ -122,7 +122,7 @@ int main()
     });
 
     // /api/get_message/:index - 取得指定訊息
-    svr.Get("/api/get_message/:index", [&](const httplib::Request &req, httplib::Response &res) {
+    svr.Post("/api/get_message/:index", [&](const httplib::Request &req, httplib::Response &res) {
         int index = std::stoi(req.path_params.at("index"));
         if(0 < index && index <= conversation.size()) {
             json j;
