cb25dc6c480096cf2c657d151e19daa7abede6f9
paul789456 <paul0965566737@gmail.com>
2025-06-15 14:40:56 +0800

    construction

--- src/client.cpp
+++ src/client.cpp
@@ -7,52 +7,46 @@ int main()
 {
     httplib::Client cli("localhost", 8080);
 
-    // 呼叫 GET /api/users/1，嘗試取得 ID 為 1 的用戶資料
-    if (auto res = cli.Get("/api/users/1"))
+    std::cout << " 0 : /help 顯示各種功能 \n";
+    while(true)
     {
-        if (res->status == 200)
+        int n;
+        std::cin >> n;
+        if(n == 0)// help
         {
-            json user_json = json::parse(res->body);
-            std::cout << "用戶1 姓名：" << user_json["name"]
-                      << "，年齡：" << user_json["age"] << std::endl;
-        }
-        else if (res->status == 404)
-        {
-            std::cout << "用戶1 不存在！" << std::endl;
-        }
-    }
-    else
-    {
-        std::cout << "GET 請求失敗\n";
-    }
 
-    // 準備新用戶的資料 JSON 並呼叫 POST /api/users 建立用戶
-    json new_user;
-    new_user["name"] = "小明";
-    new_user["age"] = 20;
-    auto res2 = cli.Post("/api/users", new_user.dump(), "application/json");
-    if (res2 && res2->status == 201)
-    {
-        json created = json::parse(res2->body);
-        std::cout << "成功建立用戶：ID=" << created["id"]
-                  << "，姓名=" << created["name"]
-                  << "，年齡=" << created["age"] << std::endl;
-    }
-    else
-    {
-        std::cout << "建立用戶失敗，狀態碼：" << (res2 ? res2->status : 0) << std::endl;
-    }
 
-    // 再次呼叫 GET /api/users/1 取得剛建立的用戶
-    if (auto res3 = cli.Get("/api/users/1"))
-    {
-        if (res3->status == 200)
-        {
-            json user_json = json::parse(res3->body);
-            std::cout << "用戶1 最新資訊：姓名=" << user_json["name"]
-                      << "，年齡=" << user_json["age"] << std::endl;
         }
-    }
+        else if (n == 1)// register
+        {
+            
+
+            
+        }
+        else if(n == 2)// user_info/:id
+        {
+
+
+        }
+        else if(n == 3)// change_info/:id
+        {
+
+        }
+        else if(n == 4)// send_Message
+        {
+
+        }
+        else if(n == 5)// get_message/:index
+        {
+
+        }
+        else if(n == 6)// end
+        {
+
+        }
+    }     
+    
+
 
     return 0;
 }
